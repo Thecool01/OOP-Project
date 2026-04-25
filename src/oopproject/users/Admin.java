dec2c0c1def97a0e3f70a7596e7f3009bf335157
MeirambekPY <zhalgasbekmeirambek06@gmail.com>
2026-04-25 16:31:46 +0500

    implemented Admin logic for user management and log monitoring

--- src/oopproject/users/Admin.java
+++ src/oopproject/users/Admin.java
@@ -1,12 +1,10 @@
 package oopproject.users;
 
+import java.util.Date;
 import oopproject.storage.DataStorage;
 
-import java.util.Date;
-
 public class Admin extends Employee {
-    public Admin() {
-    }
+    public Admin() {}
 
     public Admin(String id, String login, String password, String firstName, String lastName,
                  double salary, Date hireDate) {
@@ -14,14 +12,28 @@ public class Admin extends Employee {
     }
 
     public void addUser(User user) {
-        DataStorage.getInstance().getUsers().add(user);
+        if (user != null) {
+            DataStorage.getInstance().getUsers().add(user);
+            DataStorage.getInstance().addLog("Admin " + getLogin() + " added user: " + user.getLogin());
+        }
     }
 
     public void removeUser(User user) {
-        DataStorage.getInstance().getUsers().remove(user);
+        if (user != null) {
+            DataStorage.getInstance().getUsers().remove(user);
+            DataStorage.getInstance().addLog("Admin " + getLogin() + " removed user: " + user.getLogin());
+        }
     }
 
     public void viewLogs() {
-        System.out.println("Logs viewing is not implemented yet.");
+        System.out.println("\n--- SYSTEM ACTION LOGS ---");
+        if (DataStorage.getInstance().getActionLogs().isEmpty()) {
+            System.out.println("No logs available.");
+        } else {
+            for (String log : DataStorage.getInstance().getActionLogs()) {
+                System.out.println(log);
+            }
+        }
+        System.out.println("--------------------------\n");
     }
 }
