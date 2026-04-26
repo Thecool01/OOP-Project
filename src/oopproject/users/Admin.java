8c7b0efac70c9087856e25f4eceeaed73e1b077d
MeirambekPY <zhalgasbekmeirambek06@gmail.com>
2026-04-26 10:24:41 +0500

    Implemented data modification logic for administrators, allowing updates to names and credentials with logging

--- src/oopproject/users/Admin.java
+++ src/oopproject/users/Admin.java
@@ -4,36 +4,35 @@ import java.util.Date;
 import oopproject.storage.DataStorage;
 
 public class Admin extends Employee {
-    public Admin() {}
-
-    public Admin(String id, String login, String password, String firstName, String lastName,
-                 double salary, Date hireDate) {
+    
+    public Admin(String id, String login, String password, String firstName, String lastName, double salary, Date hireDate) {
         super(id, login, password, firstName, lastName, salary, hireDate);
     }
 
     public void addUser(User user) {
-        if (user != null) {
-            DataStorage.getInstance().getUsers().add(user);
-            DataStorage.getInstance().addLog("Admin " + getLogin() + " added user: " + user.getLogin());
-        }
+        DataStorage.getInstance().getUsers().add(user);
+        DataStorage.getInstance().addLog("Admin added new user: " + user.getLogin());
     }
 
     public void removeUser(User user) {
+        if (DataStorage.getInstance().getUsers().remove(user)) {
+            DataStorage.getInstance().addLog("Admin removed user: " + user.getLogin());
+        }
+    }
+
+    public void updateUser(User user, String newFirstName, String newLastName, String newPassword) {
         if (user != null) {
-            DataStorage.getInstance().getUsers().remove(user);
-            DataStorage.getInstance().addLog("Admin " + getLogin() + " removed user: " + user.getLogin());
+            user.setFirstName(newFirstName);
+            user.setLastName(newLastName);
+            user.setPassword(newPassword);
+            DataStorage.getInstance().addLog("Admin updated details for user: " + user.getLogin());
         }
     }
 
     public void viewLogs() {
-        System.out.println("\n--- SYSTEM ACTION LOGS ---");
-        if (DataStorage.getInstance().getActionLogs().isEmpty()) {
-            System.out.println("No logs available.");
-        } else {
-            for (String log : DataStorage.getInstance().getActionLogs()) {
-                System.out.println(log);
-            }
+        System.out.println("\n--- SYSTEM LOGS ---");
+        for (String log : DataStorage.getInstance().getLogs()) {
+            System.out.println(log);
         }
-        System.out.println("--------------------------\n");
     }
 }
