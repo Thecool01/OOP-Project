65a53873efe1a511ff1411a0071d7f973feb40c4
Utro <tomiriszazykbayeva@gmail.com>
2026-05-17 12:58:46 +0500

    change Report
    Override methods: toString(), hashCode(), equals(Object o)
    added setTyoe(), setContent
    createdAt now final

--- src/oopproject/teaching/Report.java
+++ src/oopproject/teaching/Report.java
@@ -2,13 +2,18 @@ package oopproject.teaching;
 
 import oopproject.enums.ReportType;
 
+import java.io.Serial;
 import java.io.Serializable;
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 public class Report implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
     private String reportId;
     private ReportType type;
-    private LocalDateTime createdAt = LocalDateTime.now();
+    private final LocalDateTime createdAt = LocalDateTime.now();
     private String content;
 
     public Report() {
@@ -36,11 +41,43 @@ public class Report implements Serializable {
         return content;
     }
 
+    // prints report content to console
     public void print() {
         System.out.println(exportAsText());
     }
 
+    // converts report data to readable text
     public String exportAsText() {
         return "[" + type + "] " + content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setType(ReportType type) {
+        this.type = type;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Report report)) return false;
+        return Objects.equals(reportId, report.reportId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(reportId);
+    }
+
+    @Override
+    public String toString() {
+        return "Report{" +
+                "reportId='" + reportId + '\'' +
+                ", type=" + type +
+                ", createdAt=" + createdAt +
+                ", content='" + content + '\'' +
+                '}';
     }
 }
